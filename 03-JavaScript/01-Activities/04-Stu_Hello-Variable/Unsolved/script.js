9164a98bda3826e7942eb9ea79d641da5e4afed2
Dangan <pro.1.h@outlook.com>
2022-07-26 17:09:21 -0600

    assign

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,13 @@
-// Write Your JavaScript Code Here
+// Declare person Variable
+var personName;
+
+//Assign a value
+var personName = "Hope";
+var pets= 2;
+var funFact= "I like to play games";
+
+//To combine the message with a variable value use the concatenation operator(+)
+//Logs "My name is "
+console.log ("My name is " + personName);
+console.log ("I have " + pets + "pets");
+console.log ("fun Fact: " + funFact);
