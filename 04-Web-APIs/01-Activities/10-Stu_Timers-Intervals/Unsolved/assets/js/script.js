c2012e04497e7884a087dcea1a3535a43060aa91
Dangan <pro.1.h@outlook.com>
2022-08-03 19:13:47 -0600

    Wednesday

--- 04-Web-APIs/01-Activities/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
@@ -10,10 +10,16 @@ function countdown() {
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
-    //
+  
     // YOUR CODE HERE
-    //
-  });
+    timeLeft--;
+    timerEl.textContent = timeLeft; 
+
+    if (timeLeft === 0){
+      clearInterval(timeInterval);
+      displayMessage ();
+    }
+  }, 1000);
 }
 
 // Displays the message one word at a time
