91caa7d10abae5e257aaf6706f2157f080987480
Dangan <pro.1.h@outlook.com>
2022-08-04 23:37:39 -0600

    changes

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -1,14 +1,56 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var charSymbols = ['`~!@#$%^&*()-_=+/\|}{][><)('];
+var upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+var lowerCase = ['abcdefghijklmnopqrstuvwxyz'];
+var charNumbers = ['0123456789'];
 
-// Write password to the #password input
+
+function generatePassword () {
+if (charSymbols + lowerCase + upperCase + charNumbers === 0) {
+  window.alert ('must be at least 8 characters, no more than 128 characters'
+  );
+  generatePassword();
+  }
+
+var promptSymbols = confirm('You should add a symbol');
+if (promptSymbols) {
+  passwordBox += charSymbols;
+}
+
+var promptLowercase = confirm('You should add lowercase letters');
+if (promptLowercase) {
+  passwordBox += charLowercase;
+}
+
+var promptUppercase = confirm('You should add uppercase letters');
+if (promptUppercase) {
+  passwordBox += charUppercase; 
+}
+
+var promptNumbers = confirm('You should add numbers');
+if (promptNumbers) {
+  passwordBox += charNumbers;
+}
+
+if (passwordBox === '') {
+  window.alert('Choose one of the criteria');
+  generatePassword();
+}
+
+for (var i=0; i < promptLength; i++) {
+  newPassword += passwordHolder.charAt (
+    Math.floor(Math.random() * passwordBox.length)
+  );
+}
+  return newPassword; 
+
+}
+var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
---
+++ 04-Web-APIs/01-Activities/28-Stu_Mini-Project/script.js
@@ -0,0 +1,39 @@
+var score = localStorage.getItem("score");
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var timeRemaining = document.getElementById('timeRemaining');
+
+function init () {
+    if (!score) {
+        score = {
+        wins: 0,
+        losses: 0
+        };
+    }
+    score = localStorage.getItem("score"){
+
+    )};
+}
+
+start.addEventListener("click", function (event) {
+    console.log("Triggered the Start");
+    countdown ();
+)};
+
+    
+reset.addEventListener(click, function (event)); {
+    console.log("Here");
+
+    
+
+function countdown () {
+   var time = 5;
+
+   var timeRemaining = setInterval(function () {
+    if (time > 0){
+        time--;
+        console.log(time);
+        showTime();
+    } else {
+        console.log('Timer is at ')
+    } 
