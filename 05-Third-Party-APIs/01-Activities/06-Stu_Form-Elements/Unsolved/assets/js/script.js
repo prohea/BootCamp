443fb6e8bba291b1e24d8a07c25a800f6d723f28
Dangan <pro.1.h@outlook.com>
2022-08-08 21:14:36 -0600

    updates

--- 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -2,5 +2,17 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit(event) {
+    event.preventDefault();
+   var shoppingItem = $('input[name="shopping-input]').val();
+   
+    console.log('No Shopping item filled out in form!');
+    return;
+}
+
+shoppingListEl.append('<li>' + shoppingItem + '<li>');
+
+$('input[name="shopping-input"]');
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit', handleFormSubmit);
--- 05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -2,5 +2,9 @@
 var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.children('ul').children().css('background', 'white')
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+var rowThree = rootEl.children('ul').eq(2);
+
+rowThree.children().eq(0).text('O');
